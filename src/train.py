4a49ef2f234b1fd270c180646630ca864300ca03
timvlong <timothy.long@stfc.ac.uk>
2025-12-07 20:11:15 +0000

    Improving the Model
    Added another feature, the amount (in £) associated to each transaction, to further help categorising these transactions. Provided more training and test data (now 2023 + 2024) to improve the accuracy. This has solved problems involving not having enough categories represented in the data. Final accuracy is 87%.

--- src/train.py
+++ src/train.py
@@ -3,19 +3,32 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
+from scipy.sparse import hstack
 
 
 # Converting the .csv files to pandas DataFrames.
-test_data = pd.read_csv("test_data.csv")
-train_data = pd.read_csv("train_data.csv")
+test_data = pd.read_csv("data/test_data.csv")
+train_data = pd.read_csv("data/train_data.csv")
 
 
-# Must convert the input data (the transaction descriptions) from a string to a numerical vector for the model to understand.
+# Must convert the text input data (the transaction descriptions) from a string to a numerical vector for the model to understand.
 # Using TF-IDF vectoriser. Considers the frequency of words (term frequency, TF) and importance of words (inverse document frequency, IDF).
 vectoriser = TfidfVectorizer()
-X_train = vectoriser.fit_transform(train_data["Transaction Description"])
+X_text_train = vectoriser.fit_transform(train_data["Transaction Description"])
 # Converting the test features to vectors using the vocabulary just learnt above.
-X_test = vectoriser.transform(test_data["Transaction Description"])
+X_text_test = vectoriser.transform(test_data["Transaction Description"])
+
+
+# The numerical input data (the amount credited to the account) is already suitable.
+# Re-shaping this array to ensure a suitable matrix is produced from the combination of these numerical and text features.
+X_num_train = train_data["Amount"].values.reshape(-1, 1)
+X_num_test = test_data["Amount"].values.reshape(-1, 1)
+
+
+# Combining these numerical and text features into one feature, suitable for inputting into the LogisticRegression model.
+# hstack ensures we are still using sparse matrices when combining this sparse matrix and dense vector.
+X_train = hstack([X_text_train, X_num_train])
+X_test = hstack([X_text_test, X_num_test])
 
 
 # Creating the target variables, y, for the train and test dataframes.
@@ -37,5 +50,3 @@ y_predicted = clf.predict(X_test)
 # Printing the accuracy of the model and the classification report.
 print("The accuracy of the model is: {}%.".format(100*accuracy_score(y_test, y_predicted)))
 print(classification_report(y_test, y_predicted))
-print(y_predicted)
-print(y_test)
